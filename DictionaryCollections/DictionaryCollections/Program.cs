009328d041f6af55ae68bc0ead87a7590015eb76
Amador99 <abelofvarad@gmail.com>
2017-06-23 22:39:16 +0200

    Assignment finished

--- DictionaryCollections/DictionaryCollections/Program.cs
+++ DictionaryCollections/DictionaryCollections/Program.cs
@@ -11,6 +11,31 @@ namespace DictionaryCollections
     {
         static void Main(string[] args)
         {
+            var lookup = new Dictionary<int, string>();
+            lookup[0] = "Zero";
+            lookup[1] = "One";
+            lookup[2] = "Two";
+            lookup[3] = "Three";
+            lookup[4] = "Four";
+            lookup[5] = "Five";
+            lookup[6] = "Six";
+            lookup[7] = "Seven";
+            lookup[8] = "Eight";
+            lookup[9] = "Nine";
+
+            string ourNumber = "888-555-1212";
+
+            foreach(char c in ourNumber)
+            {
+                int digit = (int)(c - '0');
+                if (lookup.ContainsKey(digit))
+                {
+                    Console.WriteLine(lookup[digit]);
+                }
+
+            }
+            Console.ReadKey();
+
         }
     }
 }
